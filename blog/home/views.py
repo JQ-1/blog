e5c1ae448910e19664343b164da759096d0c8b08
LiuJiaQi <1529448869@qq.com>
2020-10-29 21:47:46 +0800

    '首页分类数据展示以及点击高亮显示'

--- blog/home/views.py
+++ blog/home/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.views import View
+from home.models import ArticleCategory
+from django.http import HttpResponseNotFound
 
 
 class IndexView(View):
     """首页"""
     def get(self, request):
         """提供首页界面"""
-        return render(request, "index.html")
+        # 1.获取所有的分类信息
+        categories = ArticleCategory.objects.all()
+        # 2.接收用户点击的分类id  ?cat_id=xxx&page_num=xxx&page_size=xxx
+        cat_id = request.GET.get("cat_id", 1)
+        # 3.根据分类id进行分类的查询
+        try:
+            category = ArticleCategory.objects.get(id=cat_id)
+        except ArticleCategory.DoesNotExist:
+            return HttpResponseNotFound("没有此分类")
+        # 4.组织数据传递给模板
+        context = {
+            "categories": categories,
+            "category": category
+        }
+        return render(request, "index.html", context=context)
