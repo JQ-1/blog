b46416a9672da2d205f6ca9f441405fe8511f753
LiuJiaQi <1529448869@qq.com>
2020-10-31 16:49:38 +0800

    '文章详情页面展示'

--- blog/home/views.py
+++ blog/home/views.py
@@ -52,3 +52,25 @@ class IndexView(View):
             "page_num": page_num,
         }
         return render(request, "index.html", context=context)
+
+
+class DetailView(View):
+    """"详情页面展示"""
+    def get(self, request):
+        #  detail/?id=xxx&page_num=xxx&page_size=xxx
+        # 1.接收文章的id信息
+        id = request.GET.get("id")
+        # 2.根据文章id 进行文章数据的查询
+        try:
+            article = Article.objects.get(id=id)
+        except Article.DoesNotExist:
+            pass
+        # 3.查询分类数据
+        categories = ArticleCategory.objects.all()
+        # 4.组织模板数据
+        context = {
+            'categories': categories,
+            'category': article.category,
+            'article': article,
+        }
+        return render(request, "detail.html", context=context)
